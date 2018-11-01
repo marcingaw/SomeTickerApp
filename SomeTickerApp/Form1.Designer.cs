8efd7d85af56b827bff8d6a07d22fab4512bae29
Marcin Gawlik <marcin.gawlik@windowslive.com>
2018-11-01 21:41:53 +0100

    Some controls for the phase difference and speed of the animation, now a line rather than dots.

--- SomeTickerApp/Form1.Designer.cs
+++ SomeTickerApp/Form1.Designer.cs
@@ -30,32 +30,65 @@
         {
             this.components = new System.ComponentModel.Container();
             this.mainTicker = new System.Windows.Forms.Timer(this.components);
+            this.hToVRatioBar = new System.Windows.Forms.TrackBar();
+            this.oscillatorHStepBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.hToVRatioBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oscillatorHStepBar)).BeginInit();
             this.SuspendLayout();
             // 
             // mainTicker
             // 
-            this.mainTicker.Interval = 20;
+            this.mainTicker.Interval = 10;
             this.mainTicker.Tick += new System.EventHandler(this.MainTicker_Tick);
+            // 
+            // hToVRatioBar
+            // 
+            this.hToVRatioBar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.hToVRatioBar.Location = new System.Drawing.Point(831, 0);
+            this.hToVRatioBar.Maximum = 199;
+            this.hToVRatioBar.Minimum = 1;
+            this.hToVRatioBar.Name = "hToVRatioBar";
+            this.hToVRatioBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.hToVRatioBar.Size = new System.Drawing.Size(69, 563);
+            this.hToVRatioBar.TabIndex = 0;
+            this.hToVRatioBar.Value = 83;
+            // 
+            // oscillatorHStepBar
+            // 
+            this.oscillatorHStepBar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.oscillatorHStepBar.Location = new System.Drawing.Point(0, 0);
+            this.oscillatorHStepBar.Minimum = 1;
+            this.oscillatorHStepBar.Name = "oscillatorHStepBar";
+            this.oscillatorHStepBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.oscillatorHStepBar.Size = new System.Drawing.Size(69, 563);
+            this.oscillatorHStepBar.TabIndex = 1;
+            this.oscillatorHStepBar.Value = 5;
             // 
             // MainTickerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(900, 563);
+            this.Controls.Add(this.oscillatorHStepBar);
+            this.Controls.Add(this.hToVRatioBar);
             this.DoubleBuffered = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainTickerForm";
             this.Text = "Some Ticker App";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainTickerForm_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.hToVRatioBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.oscillatorHStepBar)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer mainTicker;
+        private System.Windows.Forms.TrackBar hToVRatioBar;
+        private System.Windows.Forms.TrackBar oscillatorHStepBar;
     }
 }
 
--- SomeTickerApp/Form1.cs
+++ SomeTickerApp/Form1.cs
@@ -1,93 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SomeTickerApp
-{
-    public partial class MainTickerForm : Form
-    {
+namespace SomeTickerApp {
+
+    public partial class MainTickerForm : Form {
+
         // The current values of the oscillators angles, in radians.
         private double oscillatorH = 0.0;
         private double oscillatorV = 0.0;
 
-        // The step of angle of the horizontal oscillator, in radians.
-        private const double OSCILLATOR_H_STEP = 0.05;
-
-        // The fraction of the horizontal oscillator step to be used as the
-        // vertical oscillator step, in radians.
-        private double oscillatorHToVRatio = 0.83;
-
         // The maximum count of past locations displayed in the ball trace.
-        private const int MAX_TRACE_LENGHTH = 255;
+        private const int TRACE_LENGHTH = 255;
 
         // The array with the ball trace and the last updated index in it.
-        private Tuple<double, double>[] ballTrace = new Tuple<double, double>[MAX_TRACE_LENGHTH];
-        private int lastBallTraceIndex = MAX_TRACE_LENGHTH - 1;
+        private Tuple<double, double>[] ballTrace = new Tuple<double, double>[TRACE_LENGHTH];
+        private int lastBallTraceIndex = TRACE_LENGHTH - 1;
 
-        public MainTickerForm()
-        {
-            InitializeComponent();
-        }
+        public MainTickerForm() => InitializeComponent();
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            mainTicker.Start();
-        }
+        private void Form1_Load(object sender, EventArgs e) => mainTicker.Start();
 
-        private void MainTicker_Tick(object sender, EventArgs e)
-        {
-            oscillatorH += OSCILLATOR_H_STEP;
-            if (oscillatorH >= 2 * Math.PI)
-            {
+        private void MainTicker_Tick(object sender, EventArgs e) {
+            oscillatorH += oscillatorHStepBar.Value * 0.01;
+
+            if (oscillatorH >= 2 * Math.PI) {
                 oscillatorH -= 2 * Math.PI;
             }
 
-            oscillatorV += oscillatorHToVRatio * OSCILLATOR_H_STEP;
-            if (oscillatorV >= 2 * Math.PI)
-            {
+            oscillatorV += hToVRatioBar.Value * oscillatorHStepBar.Value * 0.0001;
+
+            if (oscillatorV >= 2 * Math.PI) {
                 oscillatorV -= 2 * Math.PI;
             }
 
-            lastBallTraceIndex = (lastBallTraceIndex + 1) % MAX_TRACE_LENGHTH;
-            ballTrace[lastBallTraceIndex] = new Tuple<double, double>(Math.Sin(oscillatorH), Math.Sin(oscillatorV));
+            lastBallTraceIndex = (lastBallTraceIndex + 1) % TRACE_LENGHTH;
+            ballTrace[lastBallTraceIndex] = new Tuple<double, double>(
+                Math.Sin(oscillatorH), Math.Sin(oscillatorV));
 
             Invalidate();
         }
 
-        private void MainTickerForm_Paint(object sender, PaintEventArgs e)
-        {
+        private void MainTickerForm_Paint(object sender, PaintEventArgs e) {
             Graphics gr = e.Graphics;
             RectangleF vcb = gr.VisibleClipBounds;
 
             double width = vcb.Width;
             double height = vcb.Height;
+            int lastX = -1, lastY = -1;
 
-            for (int ctr = 1; ctr <= MAX_TRACE_LENGHTH; ctr++)
-            {
-                int idx = (lastBallTraceIndex + ctr) % MAX_TRACE_LENGHTH;
+            for (int ctr = 1; ctr <= TRACE_LENGHTH; ctr++) {
+                int idx = (lastBallTraceIndex + ctr) % TRACE_LENGHTH;
 
-                if (ballTrace[idx] != null)
-                {
-                    double x = (width / 2.0) + (width / 3.0) * ballTrace[idx].Item1;
-                    double y = (height / 2.0) - (height / 3.0) * ballTrace[idx].Item2;
+                if (ballTrace[idx] != null) {
+                    int x = (int)((width / 2.0) + (width / 3.0) * ballTrace[idx].Item1);
+                    int y = (int)((height / 2.0) - (height / 3.0) * ballTrace[idx].Item2);
 
-                    gr.DrawEllipse(
-                        new Pen(Color.FromArgb(ctr, ctr, ctr), 3.0f),
-                        new Rectangle
-                        {
-                            X = (int)(x) - 1,
-                            Y = (int)(y) - 1,
-                            Width = 3,
-                            Height = 3
-                        });
+                    if (lastX >= 0 && lastY >= 0) {
+                        gr.DrawLine(
+                            new Pen(Color.FromArgb(ctr, ctr, ctr), 3.0f),
+                            lastX, lastY, x, y);
+                    }
+
+                    lastX = x;
+                    lastY = y;
                 }
             }
         }
+
     }
+
 }
